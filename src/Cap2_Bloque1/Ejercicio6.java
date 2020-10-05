6389c3d50b9ffb5b90dea9320bfe552010dd0d82
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-10-05 13:42:26 +0200

    UP B2_Ejercicio 6

--- src/Cap2_Bloque1/Ejercicio6.java
+++ src/Cap2_Bloque1/Ejercicio6.java
@@ -1,12 +1,30 @@
 package Cap2_Bloque1;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Programa que verifica si un número es par o impar\n");
+		int numero = lectorInt();
 		
-		
-
+		if (numero % 2 != 0)
+			System.out.println("El número es IMPAR");
+		else
+			System.out.println("El número es PAR");
+	}
+	
+	
+	/**
+	 * Lee por pantalla los números
+	 * 
+	 * @param cant  	cantidad de números que se leerán
+	 * @param vectorInt	vector donde guardar los números
+	 */
+	public static int  lectorInt () {		
+		String lector = JOptionPane.showInputDialog("Introduzca el número entero: ");
+		return Integer.parseInt(lector);			
 	}
 
 }
