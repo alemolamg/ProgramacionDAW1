0f8e47a7616aa7dcdf49738e56a6e97fd8729421
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-10-05 14:22:12 +0200

    Terminado Capitulo 2 Bloque 1

--- src/Cap2_Bloque1/Ejercicio7.java
+++ src/Cap2_Bloque1/Ejercicio7.java
@@ -1,10 +1,28 @@
 package Cap2_Bloque1;
 
+import javax.swing.JOptionPane;
+
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int numUno = 1, numLeido = 0;
 
+		numLeido = lectorInt();
+		if ( (numLeido & numUno) == numUno )
+			System.out.println("El número es IMPAR");
+		else
+			System.out.println("El número es PAR");
+		
+	}
+	
+	/**
+	 * Lee por pantalla los números
+	 * 
+	 * @return (int) número leido
+	 */
+	public static int  lectorInt () {		
+		String lector = JOptionPane.showInputDialog("Introduzca el número entero: ");
+		return Integer.parseInt(lector);			
 	}
 
 }
