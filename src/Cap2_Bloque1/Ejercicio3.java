abd62cb15ce3e09ec53294346c986e475b5bce19
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-10-05 13:26:26 +0200

    ADD Ejercico 3 y 4 terminados

---
+++ src/Cap2_Bloque1/Ejercicio3.java
@@ -0,0 +1,48 @@
+package Cap2_Bloque1;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio3 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		int numNums = 5, numMayor = Integer.MIN_VALUE, numMenor = Integer.MAX_VALUE;
+		int [] numVectores = new int [numNums];
+		String lector;
+		
+		System.out.println("Calcula el número mayor y menor de una serie de números");
+				
+		lectorVectoresInt(numNums, numVectores);
+		
+		
+		for (int i = 0; i < numNums; i++) {
+			if(numVectores[i] > numMayor)
+				numMayor=numVectores[i];
+			
+//			if(numVectores[i] < numMenor)
+//				numMenor = numVectores[i];	
+		}
+		
+		System.out.println("El número mayor es: " + numMayor);
+			
+	}
+
+	/**
+	 * Lee por pantalla los números
+	 * 
+	 * @param cant  	cantidad de números que se leerán
+	 * @param vectorInt	vector donde guardar los números
+	 */
+	public static void  lectorVectoresInt (int cant, int [] vectorInt) {
+		String lector;
+		
+		for (int j = 0; j < cant; j++) {
+			lector = JOptionPane.showInputDialog("Introduzca el " + (1+j) +"º número entero: ");
+			vectorInt[j] = Integer.parseInt(lector);			
+		}
+		
+	}
+
+}
---
+++ src/Cap2_Bloque1/Ejercicio5.java
@@ -0,0 +1,5 @@
+package Cap2_Bloque1;
+
+public class Ejercicio5 {
+
+}
