43a77e41e558c5b99273b90afe525ae4d4633fab
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-10-06 19:33:34 +0200

    UP Cap2_Bloque3 Ejercicio4. Añadido vector con las monedas

--- src/Cap2_Bloque3/Ejercicio4.java
+++ src/Cap2_Bloque3/Ejercicio4.java
@@ -4,8 +4,7 @@ public class Ejercicio4 {
 
 	public static void main(String[] args) {
 		
-		int [] monedasVuelta = new int [5];
-		
+		int monedasVuelta [] = new int [] {100, 50, 25, 5, 1};
 
 	}
 
