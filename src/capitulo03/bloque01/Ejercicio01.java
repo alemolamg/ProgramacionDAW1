b6db4db60cc6900ae3b89a7f3bd25db20c9795f4
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-10-08 10:13:15 +0200

    UP Cap03_Bloque01 Ejercicio01 terminado.

--- src/capitulo03/bloque01/Ejercicio01.java
+++ src/capitulo03/bloque01/Ejercicio01.java
@@ -1,10 +1,41 @@
 package capitulo03.bloque01;
 
+import java.util.Vector;
+
+import javax.swing.JOptionPane;
+
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
+		Vector<Integer> vectorInt = new Vector<Integer>();
+		int limite = 10, cantidad = 4, total = 0;
 		
+		System.out.println("Programa que lee por pantalla números y suma aquellos que sean mayores de 10");
+		System.out.println("Lectura de números: ");
+		for (int i = 0; i < cantidad; i++) {
+			System.out.println("Introducir el " + (i+1) + "º número");
+			vectorInt.add(lectorInt());
+		}
+		
+		for (int i = 0; i < vectorInt.size(); i++) {
+			if(vectorInt.elementAt(i) > limite)
+				total += vectorInt.elementAt(i);
+		}
+		
+		System.out.println("La suma total de los números"
+				+ " mayores de " + limite + " es: " + total);
 
+	}
+	
+	
+	/**
+	 * Lee por pantalla los números
+	 * 
+	 * @return (int) número leido
+	 */
+	public static int  lectorInt () {		
+		String lector = JOptionPane.showInputDialog("Introduzca el número entero: ");
+		return Integer.parseInt(lector);			
 	}
 
 }
