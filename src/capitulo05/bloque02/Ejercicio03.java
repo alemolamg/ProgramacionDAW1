91be493aa20fc789b8c3841e4193bd5a4d1799b5
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-10-13 12:53:20 +0200

    ADD Ejercicio03 Cap5

--- src/capitulo05/bloque02/Ejercicio03.java
+++ src/capitulo05/bloque02/Ejercicio03.java
@@ -1,10 +1,29 @@
 package capitulo05.bloque02;
 
+import java.util.Random;
+
 public class Ejercicio03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int limite = 150, sumaPares = 0;
+		int array1 [] = new int [limite];
+		
+		for (int i = 0; i < limite; i++) {		//Inicializa array con números aleatorios
+			array1 [i] = generadorRandomInt();			
+		}
+		
+		for (int i = 0; i < array1.length; i++)
+			System.out.println("Posición " + i + ", valor = " + array1[i]);
+	}
+		
+	
+	/**
+	 * Genera un entero Random entre 0 y 100, y lo devuelve.
+	 * @return	(int) Random entre 0 y 100.
+	 */
+	private static int generadorRandomInt() {
+		Random r1 = new Random();
+		return r1.nextInt(101);
 	}
 
 }
