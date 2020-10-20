fb3384bf1ff20527345773ec84e9910cc3598168
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-10-20 12:43:19 +0200

    UP Cap05_Bloque03 Ejercicio03 Terminado.
    El algoritmo shell debía recorrer el vector hasta terminar de ordenarlo.

--- src/capitulo05/bloque03/Ejercicio03.java
+++ src/capitulo05/bloque03/Ejercicio03.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Ejercicio03 {
 
 	public static void main(String[] args) {
-		int iterator = 0, limite = 10, valorAux = 0;
+		int iterator = 0, limite = 1000, valorAux = 0;
 		int[] array = new int[limite];
 
 		System.out.println("Programa de ordenación por shell");
@@ -13,12 +13,14 @@ public class Ejercicio03 {
 		for (int i = 0; i < limite; i++) { // Inicializa array con números aleatorios
 			array[i] = generadorRandomInt();
 		}
+		
+//		array = new int[] {423, 384, 223, 224, 317, 118, 677, 329, 951, 529};
 
+		imprimirArray(array);		
+		
 		algoritmoShell(array, limite);
 
-		for (int i = 0; i < array.length; i++) // mostrar array por pantalla
-			System.out.println("Posición " + (i) + ", número: " + array[i]);
-	    
+		imprimirArray(array);
 	}
 	
 	
@@ -32,17 +34,35 @@ public class Ejercicio03 {
 	}
 	
 	
+	/**
+	 * 
+	 */
+	private static void imprimirArray(int array[]) {
+		for (int i = 0; i < array.length; i++) // mostrar array por pantalla
+			System.out.print(array[i] + " ");
+		System.out.println();
+	}
+	
 	private static void algoritmoShell(int array [], int limite) {
 		int division = array.length;
-		
+		boolean hayIntercambios;
 		do {
+			hayIntercambios = true;
 			division = division / 2;
-			for (int i = division; i < array.length; i++) {
-				int aux;
-				if (array[i] < array[i-division]) {
-					aux = array[i];
-					array[i] =  array[i-division];
-					array[i-division] = aux;					
+//			System.out.println("Division: " + division);
+//			imprimirArray(array);
+			while (hayIntercambios) {
+				hayIntercambios = false;
+				for (int i = division; i < array.length; i++) {
+					int aux;
+					if (array[i] < array[i-division]) {
+						aux = array[i];
+						array[i] =  array[i-division];
+						array[i-division] = aux;
+						hayIntercambios = true;
+//						System.out.print("Cambio ");
+//						imprimirArray(array);
+					}
 				}
 			}
 			
