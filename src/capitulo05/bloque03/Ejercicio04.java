25321a07965610e6f70409427af8d395db78b21b
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-10-20 13:46:11 +0200

    ADD Cap05_Bloque03 Ejercicio04 terminado.
    Ejercicio funciona correctamente.

---
+++ src/capitulo05/bloque03/Ejercicio04.java
@@ -0,0 +1,50 @@
+package capitulo05.bloque03;
+
+import utilidades.UtilsAlemol;
+
+
+public class Ejercicio04 {
+
+	public static void main(String[] args) {
+		int limite = 150;
+		int[] array = new int [limite];
+		
+		for (int i = 0; i < limite; i++) { // Inicializa array con números aleatorios
+			array[i] = UtilsAlemol.obtenerNumeroAzar();
+		}
+		
+		UtilsAlemol.imprimirArray(array);
+		
+		algoritmoSeleccion(array);
+		
+		System.out.println("El array ordenado es: ");
+		UtilsAlemol.imprimirArray(array);
+		
+	}
+	
+	
+	/**
+	 * Algoritmo que ordena el vector de menor a mayor
+	 * @param array (int[]) vector a ordenar
+	 */
+	private static void algoritmoSeleccion (int array []) {
+		int valor = 0, pos = 0, i,j;
+		
+		for (i = 0; i < array.length; i++) {
+			valor = array[i];
+			pos = i;
+			for (j = i; j < array.length; j++) {
+				if(array[j] < valor) {
+					valor = array[j];
+					pos = j;							
+				}
+			}
+			int aux = array[i];
+			array[i] = valor;
+			array[pos] = aux;
+		}
+		
+		System.out.println("Algoritmo terminado");
+	}
+
+}
