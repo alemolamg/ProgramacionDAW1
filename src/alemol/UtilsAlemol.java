1607d2c96240cb1a1a93ff25f42d2dce17832cf3
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-11-16 12:53:39 +0100

    Up UtilsAlemol.java

--- src/alemol/UtilsAlemol.java
+++ src/alemol/UtilsAlemol.java
@@ -104,7 +104,7 @@ public class UtilsAlemol {
 	 */
 	public static void imprimirMatriz (int matriz [][]) {
 		for(int i = 0; i < matriz.length; i++) {
-			for (int j = 0; j <matriz.length; j++)
+			for (int j = 0; j <matriz[i].length; j++)
 				System.out.print(matriz[i][j] + " ");
 		System.out.println("");
 		}
