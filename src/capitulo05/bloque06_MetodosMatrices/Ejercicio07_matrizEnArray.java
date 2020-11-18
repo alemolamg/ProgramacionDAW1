d6b0f3d93ec5ce661a69509e3b677f32fb487f3f
Alemol <33874617+alemolamg@users.noreply.github.com>
2020-11-18 09:28:13 +0100

    ADD Ejercicio07.java

---
+++ src/capitulo05/bloque06_MetodosMatrices/Ejercicio07_matrizEnArray.java
@@ -0,0 +1,40 @@
+package capitulo05.bloque06_MetodosMatrices;
+
+import alemol.UtilsAlemol;
+
+public class Ejercicio07_matrizEnArray {
+
+	public static void main(String[] args) {
+		int matriz[][]={{0,7,3,4},
+						{4,5,0,2},
+						{0,0,1,2},
+						{0,3,9,0}};
+		
+		UtilsAlemol.imprimirMatriz(matriz);
+		int vector[] = matrizEnArray(matriz);
+		UtilsAlemol.imprimirArray(vector);
+
+	}
+	
+	/**
+	 * Transforma una matriz en un array
+	 * @param matriz
+	 * @return
+	 */
+	public static int[] matrizEnArray(int matriz[][]) {
+		int tamanio = 0;	// tamaño del vector.
+		for (int i = 0; i < matriz.length; i++)
+			for (int j = 0;j < matriz[i].length; i++)
+				tamanio++;
+		
+		int array[] = new int [tamanio];
+		int k = 0;
+		for (int i = 0; i < matriz.length; i++)
+			for (int j = 0; j < matriz[i].length; i++) {
+				array[k] = matriz[i][j];
+				k++;
+			}		
+		return array;
+	}
+
+}
