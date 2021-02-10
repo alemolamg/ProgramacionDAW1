e38d2f693be7bf8fc7a122743b26967fd277f72d
Alemol <33874617+alemolamg@users.noreply.github.com>
2021-02-10 20:04:43 +0100

    UP ejercicio LímitesPrimitivos con Bytes añadidos

--- src/capitulo06_Recursos/wrappers/Ejercicio01_limitesPrimitivos.java
+++ src/capitulo06_Recursos/wrappers/Ejercicio01_limitesPrimitivos.java
@@ -6,18 +6,24 @@ public class Ejercicio01_limitesPrimitivos {
 		
 		System.out.println("El valor menor para Byte es: " + Byte.MIN_VALUE);
 		System.out.println("El valor mayor para Byte es: " + Byte.MAX_VALUE);
+		System.out.println("El valor en bytes para Byte es: " + Byte.BYTES);
+		
 		
 		System.out.println("El valor menor para Int es: " + Integer.MIN_VALUE);
 		System.out.println("El valor mayor para Int es: " + Integer.MAX_VALUE);
+		System.out.println("El valor en bytes para Int es: " + Integer.BYTES);
 		
 		System.out.println("El valor menor para Short es: " + Short.MIN_VALUE);
 		System.out.println("El valor mayor para Short es: " + Short.MAX_VALUE);
+		System.out.println("El valor en bytes para Short es: " + Short.BYTES);
 		
 		System.out.println("El valor menor para Long es: " + Long.MIN_VALUE);
 		System.out.println("El valor mayor para Long es: " + Long.MAX_VALUE);
+		System.out.println("El valor en bytes para Long es: " + Long.BYTES);
 		
 		System.out.println("El valor menor para Float es: " + Float.MIN_VALUE);
 		System.out.println("El valor mayor para Float es: " + Float.MAX_VALUE);
+		System.out.println("El valor en bytes para Float es: " + Float.BYTES);
 		
 		System.out.println("El valor menor para Double es: " + Double.MIN_VALUE);
 		System.out.println("El valor mayor para Double es: " + Double.MAX_VALUE);		
